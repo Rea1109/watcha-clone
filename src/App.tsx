d6d811a19611db3af7bc0ba67a079536fdb61391
rea <woohr1109@gmail.com>
2022-07-13 19:24:24 +0900

    update router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+import Movie from './pages/Movie';
+import Tv from './pages/Tv';
+import MovieDetail from './pages/MovieDetail';
+import TvDetail from './pages/TvDetail';
+
 const App = () => {
-    return <div>App</div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/movie" element={<Movie />} />
+                <Route path="/movie/:id" element={<MovieDetail />} />
+                <Route path="/tv" element={<Tv />} />
+                <Route path="/tv/:id" element={<TvDetail />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
---
+++ src/pages/TvDetail.tsx
@@ -0,0 +1,5 @@
+const TvDetail = () => {
+    return <div>TvDetail</div>;
+};
+
+export default TvDetail;
